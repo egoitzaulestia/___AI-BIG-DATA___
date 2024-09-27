e7b6ff0f2c7930aee11e75e0a5b7f124df662501
Egoitz Aulestia Padilla <2425BIGDATA401@CEBANCDEA.EDU>
2024-09-27 14:23:37 +0200

    numpy and pandas

---
+++ 01-Programacion-de-IA/02-Numpy-Pandas/Ejercicios_Numpy_Egoitz_Aulestia.py
@@ -0,0 +1,145 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Ejercicios NumPy
+
+# #### Importar NumPy como np
+
+import numpy as np
+
+# #### Generar un array de 10 ceros
+
+np.zeros(10)
+np.full(10, 6)
+
+# #### Generar un array con 10 unos
+
+np.ones(10)
+
+# #### Generar un array con 10 seises
+
+6 * np.ones(10)
+
+
+# #### Generar un array de números enteros entre el 20 y el 40 (incluidos ambos)
+
+np.arange(20, 41)
+
+
+
+# #### Generar un array con todos los números pares desde el 15 al 59
+
+np.arange(15, 60, 2)
+
+
+
+# #### Generar una matriz de 4x3 cuyos valores estén comprendidos entre el 0 y el 11
+
+arr = np.arange(0, 12)
+
+my_matrix = arr.reshape(4,3)
+
+my_matrix
+
+
+
+# #### Generar una matriz identidad de 4x4
+
+np.eye(4)
+
+# #### Generar un número aleatorio entre 0 y 1
+
+np.random.rand(1)
+
+
+# #### Generar un número aleatorio entre 0 y 2
+
+np.random.rand(2)
+
+
+# #### Usar NumPy para generar un array de 25 números aleatorios extraídos de 
+# una distribución normal.
+
+arr_1 = np.random.randn(25)
+
+arr_1
+
+
+# #### Generar una matriz de 10x10 cuyos valores estén comprendidos 
+# entre 0.01 y 1, en incrementos de 0.01:
+
+
+arr_2 = np.linspace(0.01, 1, 100)
+
+arr_2.reshape(10, 10)
+
+arr_2
+
+
+# #### Generar un array de 20 elementos comprendidos entre el 0 y 1.
+
+arr_3 = np.linspace(0, 1, 20)
+arr_3
+
+
+# ## Indexación y selección en Numpy
+
+# #### Generar un matriz de 5x5, con valores correlativos entre 1 y 25.  
+# Guardar dicho array en el objeto "mat"
+
+arr_4 = np.arange(1, 26)
+mat = arr_4.reshape(5,5)
+mat
+
+
+# #### Mostrar los datos contenidos en mat, a partir de la fila 3 y la columna 2 
+# (tened en cuenta que existen la fila 0 y la columna 0).
+
+mat
+mat[2:, 1:]
+
+
+# #### Extraer el valor de la celda que situado entre la cuarta fila y la quinta columna
+
+mat[3, 3]
+
+
+# #### Extraer los valores comprendidos en las 2 primeras filas y las columnas 2 y 3
+
+mat
+mat[:2,1:3]
+
+
+# #### Extraer los elementos de la quinta fila
+
+mat[:,4]
+mat[:,-1]
+
+
+# #### Extraer los elementos de las filas 4 y 5
+
+mat
+mat[3:,:]
+
+
+# #### Sumar todos los elementos existentes en mat
+
+sum(sum(mat))
+
+# #### Calcular la desviación estándar del array mat
+
+mat.std()
+
+
+# #### Calcular la suma de cada columna en mat
+
+sum(mat)
+
+
+# #### Calcular la suma de cada fila en mat
+
+mat.sum(axis=1)
+
+
+
+
